c74b497d8b5acfc94efba6ebb931aa36ccbfd9f8
KHUSHI 444 <khankhushi4452@gmail.com>
2024-08-02 23:08:07 +0530

    basic setup

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
    <div>
     <h2>
       Welcome to this Website......
+      <Button>
+       Subscribe
+      </Button>
     </h2>
    </div>
   );
