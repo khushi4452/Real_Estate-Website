e901a01599aa51c01e21afe85ac8c5f717570b59
KHUSHI 444 <khankhushi4452@gmail.com>
2024-08-09 21:55:00 +0530

    added logo and content

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -5,6 +5,12 @@ const Header = () => {
   return (
     <div>
       <img src='/Logo.svg' width={150} height={150} alt='Logo' />
+      <ul>
+        <li>
+          For sale 
+          
+        </li>
+      </ul>
     </div>
   );
 };
